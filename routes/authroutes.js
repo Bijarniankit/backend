d28a4720bb7266ffc54101b02a7bb51731324eb5
Bijarniankit <108739009+Bijarniankit@users.noreply.github.com>
2024-08-04 22:41:37 +0530

    Update authroutes.js

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -3,7 +3,7 @@ import paths from '../controllers/authcontroller.js';
 import validator from '../middleware/validationmiddleware.js';
 const routes = express.Router();
 
-routes.post('/signin', validator.validateSignin, paths.signin);
-routes.post('/signup' , validator.validateSignup , paths.signup);
+routes.post('/signin', paths.signin);
+routes.post('/signup' , paths.signup);
 
 export default routes;
