ec259183763d25ab8e773cc7b7246f6a71ca7089
Bijarniankit <ankitbijarnia98@gmail.com>
2024-08-04 22:22:59 +0530

    Added validation

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import paths from '../controllers/authcontroller.js';
+import validator from '../middleware/validationmiddleware.js';
 const routes = express.Router();
 
-routes.post('/signin', paths.signin);
-routes.post('/signup', paths.signup);
+routes.post('/signin', validator.validateSignin, paths.signin);
+routes.post('/signup' , validator.validateSignup , paths.signup);
 
 export default routes;
